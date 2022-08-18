eee4b5219d0218f9e2c1d346ca7b6609954ac86c
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-08-18 15:34:51 -0400

    draw bitmaps

--- Display.java
+++ Display.java
@@ -1,10 +1,20 @@
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.BufferStrategy;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class Display extends Canvas {
 
-    public final JFrame m_frame;
+    private final JFrame m_frame;
+    private final Bitmap m_frameBuffer;
+    private final BufferedImage m_displayImage;
+    private final byte[] m_displayComponents;
+    private final BufferStrategy m_bufferStrategy;
+    private final Graphics m_graphics;
 
     public Display(int width, int height, String title) {
 
@@ -12,6 +22,12 @@ public class Display extends Canvas {
         setPreferredSize(size);
         setMinimumSize(size);
         setMaximumSize(size);
+
+        m_frameBuffer = new Bitmap(width, height);
+        m_displayImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        m_displayComponents = ((DataBufferByte) m_displayImage.getRaster().getDataBuffer()).getData();
+
+        m_frameBuffer.Clear((byte) 0x80);
 
         m_frame = new JFrame();
         m_frame.add(this);
@@ -21,5 +37,15 @@ public class Display extends Canvas {
         m_frame.setLocationRelativeTo(null);
         m_frame.setTitle(title);
         m_frame.setVisible(true);
+
+        createBufferStrategy(1);
+        m_bufferStrategy = getBufferStrategy();
+        m_graphics = m_bufferStrategy.getDrawGraphics();
+    }
+
+    public void SwapBuffers() {
+        m_frameBuffer.CopyToByteArray(m_displayComponents);
+        m_graphics.drawImage(m_displayImage, 0, 0, m_frameBuffer.getWidth(), m_frameBuffer.getHeight(), null);
+        m_bufferStrategy.show();
     }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         Display display = new Display(800, 600, "Software Renderer");
+
+        while (true) {
+            display.SwapBuffers();
+        }
     }
 
 }
