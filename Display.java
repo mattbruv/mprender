6c422d6542f1fba308f4c6fe0e742dac5d7e0f22
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-08-18 15:16:25 -0400

    start java renderer

---
+++ Display.java
@@ -0,0 +1,25 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Display extends Canvas {
+
+    public final JFrame m_frame;
+
+    public Display(int width, int height, String title) {
+
+        Dimension size = new Dimension(width, height);
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setMaximumSize(size);
+
+        m_frame = new JFrame();
+        m_frame.add(this);
+        m_frame.pack();
+        m_frame.setResizable(false);
+        m_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        m_frame.setLocationRelativeTo(null);
+        m_frame.setTitle(title);
+        m_frame.setVisible(true);
+    }
+}
